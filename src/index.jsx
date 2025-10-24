134d8f85c186ef02d594d278ed2ae1442f63224c
Era-smus <erasmussaat1@gmail.com>
2025-10-24 10:23:58 +0300

    Modified index.js

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import routes from './routes';
+import './index.css';
+
+const router = createBrowserRouter(routes);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
